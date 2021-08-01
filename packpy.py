8536aef5b41ca4203da6151e4e7504d50c7fe8eb
kerem3338 <73388608+kerem3338@users.noreply.github.com>
2021-08-01 22:22:24 +0300

    version 0.1

---
+++ packpy.py
@@ -0,0 +1,37 @@
+#2021 Kakos lkişselleştirilmiş lisans © Zoda
+#Bu kodu kişsel kullanım için modifiye edip yayımlayabilirsiniz
+
+import json
+import os
+import sys
+
+def add_conf_file(filename):
+  file = open(filename, "r")
+  filedata = json.load(file)
+
+  try:
+    os.chdir(filedata["install_path"])
+  except KeyError:
+    print(f"{filename} dosyasında install_path bulunamadı.")
+  except FileNotFoundError:
+    print(f"{filename} dosyasında belirtilen {filedata['install_path']} klasörü bulunamadı.")
+  
+
+  package_list = {
+    "Python": "apt install python",
+    "python": "apt install python",
+    "c": "apt install clang",
+    "ruby": "apt install ruby",
+    "php": "apt install php"
+  }
+  
+  
+  try:
+    if filedata["install_package"] in package_list:
+      os.system(filedata["install_package"])
+  except KeyError:
+    print("İnirilecek paket bulunamadı")
+
+if __name__ == "__main__":
+  add_conf_file(sys.argv[1])
+
